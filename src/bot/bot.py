d5c5782c4eb85384d121ed37d4204d84c31fed1d
TheSoupMasta <78989674+TheSoupMasta@users.noreply.github.com>
2025-05-27 22:50:12 -0400

    fixed bugs with permissions

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -5,10 +5,14 @@ import os
 import json
 from discord.ext import commands
 from discord import VoiceChannel
+# import PyNaCl
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.messages = True
+intents.guilds = True
+# intents.members = True
+intents.guild_messages = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 #write these into a file to make the bot restart-proof
@@ -17,7 +21,7 @@ monitored_roles = {}
 
 TARGET_GUILD_ID = ""
 USER_ID = ""
-BOT_TOKEN = ""
+BOT_TOKEN = "MTM2Nzg5NTE1ODY5OTMzMTY1NA.Gutq4l.6MuxMZFpWGSw5nk4H1Aszcakb6ZIRSWwYdWxag"
 LOG_FILE = ""
 
 async def ratelimit_safe(coro):
@@ -35,19 +39,26 @@ async def ratelimit_safe(coro):
 @bot.command(name="join", help="Joins the channel you (invoker of the command) are currently in.")
 async def vc_connect(ctx):
     if ctx.guild.id in monitored_roles.keys() or ctx.author.guild_permissions.administrator or ctx.author.guild_permissions.manage_guild:
-        check_role = monitored_roles[ctx.guild.id]
-        if check_role in ctx.author.roles or ctx.author.guild_permissions.manage_guild or ctx.author.guild_permissions.administrator:
+        check_role = monitored_roles.get(ctx.guild.id)
+        if discord.utils.get(ctx.guild.roles, id=check_role) or ctx.author.guild_permissions.manage_guild or ctx.author.guild_permissions.administrator:
             if ctx.author.voice and ctx.author.voice.channel:
                 vc = ctx.author.voice.channel
                 await vc.connect()
-                await ctx.send("Joined channel!")
+                # await ctx.send("Joined channel!")
             else:
                 await ctx.send("Please join a channel first!")
         else:
             await ctx.send("Improper Permissions! Please contact a server admin to setup role permissions")
     else:
-        await ctx.send("Please setup role permissions first!")
+        await ctx.send("You do not have the proper permissions for this command, or setup role permissions first!")
 
+@bot.command(name="stop", help="Forces the bot to leave the voice channel it is in.")
+async def vc_disconnect(ctx):
+    vc = ctx.author.voice.channel
+    if vc:
+        await vc.voice_client.disconnect()
+    else:
+        ctx.send("Please execute this command from inside the voice channel the bot is in.")
 
 @bot.command(name="change_role_permissions", help="Changes what roles the bot listens to, aside from those with administrator permissions or manage server permissions.")
 async def change_perms(ctx):
@@ -81,7 +92,7 @@ async def on_guild_join(guild):
                              "On top of this role, anyone with administrator or manage server permissions can still access me. \n"
                              "Please mention the role I should monitor (e.g., @Admin):")
         except discord.Forbidden:
-            if guild.system_channels:
+            if guild.system_channel:
                 await guild.system_channel.send(f"{owner.mention}, please mention the role I should monitor (e.g., @Admin): \n"
                                                 "On top of this role, anyone with administrator or manage server permissions can still access me. \n")
     else:
@@ -90,21 +101,36 @@ async def on_guild_join(guild):
 @bot.event
 async def on_message(message):
     if not (message.author == bot.user):
-        if message.guild is None and message.author.id in pending_role_setup:
+        if message.author.id in pending_role_setup:
             guild_id = pending_role_setup[message.author.id]
             guild = bot.get_guild(guild_id)
             if guild is None:
                 return
-        if message.role_mentions:
-            role = message.role_mentions[0]
-            monitored_roles[guild.id] = role.id
-            await message.channel.send(f"Great! I'll monitor the **{role.name}** role.")
-            del pending_role_setup[message.author.id]
 
-# @bot.event
-# async def on_ready():
-#
-#     await bot.close()
+            role_name = message.content.strip().lstrip("@")  # Clean up "@Admin" to "Admin"
+            role = discord.utils.find(lambda r: r.name == role_name, guild.roles)
+
+            if role:
+                monitored_roles[guild.id] = role.id
+                await message.channel.send(f"Great! I'll monitor the **{role.name}** role.")
+                del pending_role_setup[message.author.id]
+            else:
+                await message.channel.send(
+                    f"Sorry, I couldn't find a role named '{role_name}'. Make sure you typed it exactly.")
+
+    await bot.process_commands(message)
+
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong!")
+    await ctx.send(monitored_roles)
+
+
+@bot.event
+async def on_ready():
+    pass
+    # await bot.close()
 
 bot.run(
     BOT_TOKEN
