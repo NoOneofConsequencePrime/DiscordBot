9a127fdeeffce6c81762e31f8dd159a7358b1f60
TheSoupMasta <78989674+TheSoupMasta@users.noreply.github.com>
2025-06-03 01:19:21 -0400

    used dotenv to obfuscate token, prevent future token leaks (of the many that already happened)

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,11 @@
 from src.bot.libs.events import *
+from dotenv import load_dotenv
+
+load_dotenv()
 
 TARGET_GUILD_ID = ""
 USER_ID = ""
-BOT_TOKEN = ""
+BOT_TOKEN = os.getenv("API_KEY")
 LOG_FILE = ""
 
 bot.run(
